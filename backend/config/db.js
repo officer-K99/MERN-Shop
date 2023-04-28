505f9778dfa6a19e0f9e68d49a72bee2fd53812c
amirreza <amirfasihi78@gmail.com>
2023-04-28 23:03:51 +0430

    add models

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`mongoDB connected: ${connect.connection.host}`.cyan.bgGreen);
+    console.log(`mongoDB connected: ${connect.connection.host}`.cyan.bgYellow);
   } catch (error) {
     console.log(`error: ${error.message}`.cyan.bgRed);
     process.exit(1);
